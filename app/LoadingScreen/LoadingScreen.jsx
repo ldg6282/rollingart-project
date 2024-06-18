0fa1813a2aa754c1f3db8c3b710024cbdf07a627
ldg <ldg6282@naver.com>
2024-06-18 16:24:25 +0900

    Rename: square.png 파일 circle.png로 파일명 수정

--- app/LoadingScreen/LoadingScreen.jsx
+++ app/LoadingScreen/LoadingScreen.jsx
@@ -1,13 +1,13 @@
 import { View, Image, Text, StyleSheet } from "react-native";
 import { vw, vh } from "react-native-expo-viewport-units";
 
-import squareImage from "../../assets/images/square.png";
+import circleImage from "../../assets/images/circle.png";
 
 export default function LoadingScreen() {
   return (
     <View style={styles.loadingContainer}>
       <Text style={styles.loadingText}>Loading...</Text>
-      <Image style={styles.squareImage} source={squareImage} />
+      <Image style={styles.circleImage} source={circleImage} />
     </View>
   );
 }
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#0F0F0F",
   },
-  squareImage: {
+  circleImage: {
     width: vw(50),
     height: vh(30),
     resizeMode: "contain",
