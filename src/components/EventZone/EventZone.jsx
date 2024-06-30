025ae2956865194f6ecd5cee451f66aa6ea85dc8
YujinKang1024 <yujinkang1024@gmail.com>
2024-06-30 21:15:18 +0900

    Refactor: collider, startZone, endZone 투명화

--- src/components/EventZone/EventZone.jsx
+++ src/components/EventZone/EventZone.jsx
@@ -3,7 +3,7 @@ import { Box } from "@react-three/drei";
 export default function EventZone({ zoneRef, position, boxColor, size, rotation }) {
   return (
     <Box ref={zoneRef} args={size} position={position} rotation={rotation}>
-      <meshStandardMaterial args={[{ color: boxColor, transparent: true, opacity: 0.3 }]} />
+      <meshStandardMaterial args={[{ color: boxColor, transparent: true, opacity: 0 }]} />
     </Box>
   );
 }
