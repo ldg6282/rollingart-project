470897e29a422bb37105409e64cab3c060a25060
ldg6282 <ldg6282@naver.com>
2024-08-15 14:32:49 +0900

    Feat: 자주 사용하는 모킹 코드 jest-setup.js 파일에 추가

--- jest-setup.js
+++ jest-setup.js
@@ -1,7 +1,52 @@
+import mockAsyncStorage from "@react-native-async-storage/async-storage/jest/async-storage-mock";
+
+jest.mock("react-native", () => {
+  const RN = jest.requireActual("react-native");
+
+  RN.Animated.timing = () => ({ start: (callback) => callback && callback() });
+  RN.Platform.OS = "android";
+  RN.BackHandler = {
+    exitApp: jest.fn(),
+  };
+
+  RN.AppState = {
+    currentState: "active",
+    addEventListener: jest.fn((event, handler) => {
+      if (event === "change") {
+        handler("background");
+      }
+      return {
+        remove: jest.fn(),
+      };
+    }),
+  };
+
+  return RN;
+});
+
+jest.mock("@react-native-async-storage/async-storage", () => mockAsyncStorage);
+
+jest.mock("react-native-expo-viewport-units", () => ({
+  vw: jest.fn((val) => val),
+  vh: jest.fn((val) => val),
+}));
+
+jest.mock("@react-three/fiber", () => ({
+  Canvas: ({ children }) => <mock-canvas>{children}</mock-canvas>,
+  useFrame: jest.fn(),
+}));
+
 jest.mock("three", () => {
   const actualThree = jest.requireActual("three");
   return {
     ...actualThree,
+    TextureLoader: jest.fn().mockImplementation(() => ({
+      load: jest.fn(() => ({
+        wrapS: "",
+        wrapT: "",
+        repeat: { set: jest.fn() },
+      })),
+    })),
     Vector3: jest.fn().mockImplementation(() => ({
       x: 0,
       y: 0,
@@ -19,13 +64,20 @@ jest.mock("three", () => {
       z: 0,
       w: 0,
     })),
-    TextureLoader: jest.fn().mockImplementation(() => ({
-      load: jest.fn().mockImplementation(() => ({
-        wrapS: "",
-        wrapT: "",
-        repeat: { set: jest.fn() },
-      })),
-    })),
     RepeatWrapping: "RepeatWrapping",
   };
 });
+
+jest.mock("react-native-reanimated", () => {
+  const Reanimated = require("react-native-reanimated/mock");
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
+
+jest.mock("@react-three/drei/native", () => ({
+  useGLTF: jest.fn(),
+}));
+
+jest.mock("expo-router", () => ({
+  useRouter: jest.fn(),
+}));
--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFiles: ["<rootDir>/jest-setup.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-three/drei)",
   ],
   collectCoverage: true,
   collectCoverageFrom: [
