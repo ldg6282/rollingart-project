662d3db44940f67b53adeac7c9eba0bb5a47c4be
YujinKang1024 <yujinkang1024@gmail.com>
2024-08-15 15:33:01 +0900

    Chore: 프로젝트에 맞추어 jest 환경 설정

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "jest-expo",
-  testEnvironment: "jsdom",
   setupFiles: ["<rootDir>/jest-setup.js"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-three/drei)",
@@ -14,9 +13,7 @@ module.exports = {
     "!src/reportWebVitals.js",
     "!**/node_modules/**",
     "!**/vendor/**",
-    "!src/hooks/**",
     "!src/styles/**",
-    "!src/utils/**",
   ],
   coverageReporters: ["text", "lcov", "clover"],
   coverageDirectory: "<rootDir>/coverage",
